314f062deee2e3cdd50b5f700ab681dc01ce70cc
Ankit Modi <ankitmodi31198@gmail.com>
2021-07-12 22:16:34 +0530

    feat(login): added login with gmail code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  SocialAuthService,
+  GoogleLoginProvider,
+  SocialUser,
+} from 'angularx-social-login';
+
 @Component({
   selector: 'sevenx-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  socialUser: SocialUser;
 
-  ngOnInit(): void {
+  isLoggedin: boolean = false;
+
+  googleLoginOptions = {
+    scope: 'profile email',
+  }; // https://developers.google.com/api-client-library/javascript/reference/referencedocs#gapiauth2clientconfig
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private socialAuthService: SocialAuthService
+  ) {}
+
+  ngOnInit() {
+    this.initLoginForm();
+    this.socialAuthService.authState.subscribe((user) => {
+      this.socialUser = user;
+      this.isLoggedin = user != null;
+      console.log(this.socialUser);
+    });
   }
 
+  initLoginForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  loginWithGoogle(): void {
+    this.socialAuthService
+      .signIn(GoogleLoginProvider.PROVIDER_ID, this.googleLoginOptions)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((data) => {
+        this.socialAuthService.signOut();
+        console.log('error');
+        console.log(data);
+      });
+  }
+
+  logOut(): void {
+    this.socialAuthService.signOut();
+  }
 }
