f978b93e174ae1646cae46eba1ae9e41639ffcb2
Ankit Modi <ankitmodi31198@gmail.com>
2021-07-18 19:36:19 +0530

    feat(account-setting): made account setting page

--- src/app/main/my-account/account-setting/account-setting.component.ts
+++ src/app/main/my-account/account-setting/account-setting.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'sevenx-account-setting',
   templateUrl: './account-setting.component.html',
-  styleUrls: ['./account-setting.component.scss']
+  styleUrls: ['./account-setting.component.scss'],
 })
 export class AccountSettingComponent implements OnInit {
+  
+  accountSettingForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.initAccountSettingForm();
   }
 
+  ngOnInit(): void {}
+
+  initAccountSettingForm() {
+    this.accountSettingForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      contactNumber: ['', Validators.required],
+      currentPassword: ['', Validators.required],
+      newPassword: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+
+  accountSettingSaveHandler() {
+    console.log('save clicked');
+  }
+
+  changePasswordControlType(passwordControlType: string, passwordInputElement: any) {
+    if (passwordInputElement) {
+      passwordInputElement.type = passwordControlType;
+    }
+  }
 }
--- src/app/main/my-account/account-setting/account-setting.module.ts
+++ src/app/main/my-account/account-setting/account-setting.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountSettingComponent } from './account-setting.component';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from 'src/shared/material.module';
 
 @NgModule({
   declarations: [
     AccountSettingComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    MaterialModule
   ],
   exports: [
     AccountSettingComponent
