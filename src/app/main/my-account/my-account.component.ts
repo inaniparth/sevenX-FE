436fe06a6c43fea92c18d6254b10f152097a47be
Ankit Modi <ankitmodi31198@gmail.com>
2021-07-18 12:50:58 +0530

    fix(header, my-account): added logout functionality

--- src/app/main/my-account/my-account.component.ts
+++ src/app/main/my-account/my-account.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { VerticalTabViewConfig } from 'src/common-ui/vertical-tab-view/vertical-tab-view.config';
 import { AccountSettingComponent } from 'src/app/main/my-account/account-setting/account-setting.component';
+import { LocalstorageService } from 'src/app/service/local-storage/localstorageservice.service';
+import { Router } from '@angular/router';
+import { LocalStorageKeyTypes } from 'src/app/service/local-storage/local-storage-key-types';
 
 @Component({
   selector: 'sevenx-my-account',
   templateUrl: './my-account.component.html',
-  styleUrls: ['./my-account.component.scss']
+  styleUrls: ['./my-account.component.scss'],
 })
 export class MyAccountComponent implements OnInit {
 
   verticalTabViewConfig: VerticalTabViewConfig;
 
-  constructor() { }
+  constructor(
+    private localStorageService: LocalstorageService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.verticalTabViewConfig = {
@@ -20,28 +26,35 @@ export class MyAccountComponent implements OnInit {
           label: 'My Orders',
           code: 'MY_ORDERS',
           iconClass: 'shopping_cart',
-          viewTemplate: AccountSettingComponent
+          viewTemplate: AccountSettingComponent,
         },
         {
           label: 'Account Settings',
           code: 'ACCOUNT_SETTINGS',
           iconClass: 'fa fa-user',
-          viewTemplate: AccountSettingComponent
+          viewTemplate: AccountSettingComponent,
         },
         {
           label: 'Upload Documents',
           code: 'UPLOAD_DOCUMENTS',
           iconClass: 'fa fa-upload',
-          viewTemplate: AccountSettingComponent
+          viewTemplate: AccountSettingComponent,
         },
         {
           label: 'Logout',
           code: 'LOGOUT',
           iconClass: 'fa fa-power-off',
-          onTabSelection: () => console.log('logout click handler')
-        }
-      ]
-    }
+          onTabSelection: this.logOutClickHandler.bind(this),
+        },
+      ],
+    };
   }
 
+  logOutClickHandler() {
+    this.localStorageService.removeLocalStorage(LocalStorageKeyTypes.TOKEN);
+    this.localStorageService.removeLocalStorage(
+      LocalStorageKeyTypes.LOGIN_USER
+    );
+    this.router.navigate(['/']);
+  }
 }
