237f87e33b1552271369f187165ebdbdf89c2633
sumeet.rana <sumeet.rana@mosaicwellness.in>
2021-07-18 13:33:07 +0530

    feat(home): changes for landing page (cards and plans) integration

--- src/app/main/home/home.module.ts
+++ src/app/main/home/home.module.ts
@@ -4,18 +4,22 @@ import { HomeComponent } from './home.component';
 import { BannerComponent } from './banner/banner.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ReasonsToChooseComponent } from './reasons-to-choose/reasons-to-choose.component';
-
-
+import { PlansComponent } from './plans/plans.component';
+import {IvyCarouselModule} from 'angular-responsive-carousel';
+import { CardModule } from './plans/card/card.module';
 
 @NgModule({
   declarations: [
     HomeComponent,
     BannerComponent,
     AboutUsComponent,
-    ReasonsToChooseComponent
+    ReasonsToChooseComponent,
+    PlansComponent,
   ],
   imports: [
     CommonModule,
+    IvyCarouselModule,
+    CardModule
   ],
   exports: [HomeComponent]
 })
