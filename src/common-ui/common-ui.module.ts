bba139a83b2aa99c09e55a4c90557361865792bc
Parth Inani <inaniparth@gmail.com>
2021-07-31 20:15:15 +0530

    Made common UI changes.

--- src/common-ui/common-ui.module.ts
+++ src/common-ui/common-ui.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VerticalTabViewModule } from './vertical-tab-view/vertical-tab-view.module';
+import { GrowlModule } from './growl/growl.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    VerticalTabViewModule
+    VerticalTabViewModule,
+    GrowlModule
   ],
   exports: [
-    VerticalTabViewModule
+    VerticalTabViewModule,
+    GrowlModule
   ]
 })
 export class CommonUiModule { }
