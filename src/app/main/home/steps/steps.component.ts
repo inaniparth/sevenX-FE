8b9ef2c827c14c33a6275ddcaeaff86ed5bc08a0
Parth Inani <inaniparth@gmail.com>
2022-04-02 21:32:49 +0530

    7xStartup steps animation changes.

--- src/app/main/home/steps/steps.component.ts
+++ src/app/main/home/steps/steps.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'sevenx-steps',
@@ -6,6 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./steps.component.scss']
 })
 export class StepsComponent implements OnInit {
+
+  bannerBottomAnimationOption: AnimationOptions = {
+    path: 'assets/idea_analysis.json'
+  };
+
+  ideaAnalysisAnimationOption: AnimationOptions = {
+    path: 'assets/idea_analysis.json'
+  };
+
+  coFounderAnimationOption: AnimationOptions = {
+    path: 'assets/co_founder.json'
+  };
+
+  incorporateStartupAnimationOption: AnimationOptions = {
+    path: 'assets/incorporate_your_startup.json'
+  };
+
+  websiteDevelopmentAndDigitalMarketingAnimationOption: AnimationOptions = {
+    path: 'assets/wesite_development_digital_marketing.json'
+  };
+
+  legalComplianceAnimationOption: AnimationOptions = {
+    path: 'assets/legal_compliance.json'
+  };
+
+  startupValuationAnimationOption: AnimationOptions = {
+    path: 'assets/startup_valuation.json'
+  };
+
+  fundingAnimationOption: AnimationOptions = {
+    path: 'assets/funding.json'
+  };
 
   constructor() { }
 
--- src/app/main/home/steps/steps.module.ts
+++ src/app/main/home/steps/steps.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { LottieModule } from 'ngx-lottie';
+import { playerFactory } from '../home.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    LottieModule.forRoot({ player: playerFactory }),
   ]
 })
 export class StepsModule { }
